1e5a973955d801b88cdb7c0afae602ea09e91506
matt <mateush.study@gmail.com>
2024-11-28 14:36:18 -0300

    feat: requirements and account model

---
+++ src/model/account.py
@@ -0,0 +1,11 @@
+class Account:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.followers = {}
+
+    def add_follower(self, id, name):
+        self.followers[id] = Account(id, name)
+
+    def get_followers(self):
+        return [follower.name for follower in self.followers.values()]
