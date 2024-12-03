8350410ce4f6854d3257bd16c6e4d86ae8f751f4
matt <mateush.study@gmail.com>
2024-12-03 13:50:07 -0300

    feat: followers

--- src/api_methods/get_followers.py
+++ src/api_methods/get_followers.py
@@ -1,18 +1,25 @@
+from time import sleep
+
 from atproto_client.models.app.bsky.graph.get_followers import Params
+from jedi.debug import reset_time
 
 from model.client import AtProtoClientContextManager
 
 
-def get_followers(actor_did: str, limit: int = 100):
-    params = Params(actor=actor_did, limit=limit)
+def get_followers(actor_did: str, max_followers: int):
+    params = Params(actor=actor_did, limit=100)
     all_followers = []
 
     with AtProtoClientContextManager() as client:
-        while True:
+        curr_num_followers = 0
+
+        while curr_num_followers < max_followers or max_followers == 0:
             response = client.app.bsky.graph.get_followers(params=params)
 
             all_followers.extend(response.followers)
             print(f"Followers retrieved: {len(response.followers)}")
+            curr_num_followers += len(response.followers)
+            sleep(0.5)
 
             if response.cursor:
                 params.cursor = response.cursor
@@ -21,3 +28,9 @@ def get_followers(actor_did: str, limit: int = 100):
 
     print(f"Total followers: {len(all_followers)}")
     return all_followers
+
+
+def get_followers_count(actor_did: str):
+    with AtProtoClientContextManager() as client:
+        response = client.get_profile(actor_did)
+        return response.followers_count
